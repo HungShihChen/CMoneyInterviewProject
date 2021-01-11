fd3d412de5ff620aef861e7548c558d405728202
Hamber Hung 洪士程 <1704003@iisigroup.com>
2021-01-12 00:39:31 +0800

    加入時間轉換共用函式

---
+++ WindowsForm/TwseTradingExchangeForms/Ext/ExtDateTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TwseTradingExchangeForms.Ext
+{
+    public static class ExtDateTime
+    {
+        public static DateTime ConvertToYmd(this DateTime value)
+        {
+            DateTime ret = new DateTime(value.Year, value.Month, value.Day);
+            return ret;
+        }
+    }
+}
