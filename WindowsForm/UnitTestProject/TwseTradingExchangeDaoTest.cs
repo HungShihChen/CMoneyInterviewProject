14653bf1997b98320eecafd1251dbab65eb54c0d
Hamber Hung 洪士程 <1704003@iisigroup.com>
2021-01-13 23:29:24 +0800

    測試改為對Dao的功能進行

---
+++ WindowsForm/UnitTestProject/TwseTradingExchangeDaoTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwseTradingExchangeForms.Data.DAO;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TwseTradingExchangeDaoTest
+    {
+        private static TwseTradingExchangeDAO dao = new TwseTradingExchangeDAO();
+        [TestMethod]
+        public void TestGetData()
+        {
+            var data = dao.GetData(new DateTime(2021, 1, 8), new DateTime(2021, 1, 8));
+            Assert.AreEqual(942, data.Count);
+        }
+
+        [TestMethod]
+        public void TestGetDataWithID()
+        {
+            var data = dao.GetData("1102", new DateTime(2021, 1, 8), new DateTime(2021, 1, 10));
+            Assert.AreEqual(1, data.Count);
+        }
+    }
+}
